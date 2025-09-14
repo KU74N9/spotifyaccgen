fe8c50aa62e24f23690ece5207673966f69a39d9
KU74N9 <66903770+KU74N9@users.noreply.github.com>
2025-09-14 10:11:37 +0700

    Update spotify.sh

--- spotify.sh
+++ spotify.sh
@@ -16,7 +16,7 @@ echo ""
 printf "$cyan ======================================= \n"
 printf "$yell      -+SPOTIFY ACCOUNT CREATOR+-\n"
 printf "$cyan ======================================= \n"
-printf "$red        CREATED BY$white @BangBurnok_TECH243 \n"
+printf "$red        CREATED BY$white @BangBurnok_TECHKA \n"
 printf "$cyan ======================================= \n"
 printf "$white"
 cat <<EOF
